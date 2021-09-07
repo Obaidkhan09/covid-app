04adf51612f9cd8ca81c3a06c466f77ebf480968
Obaid Ur Rahman <obaidkhan972@gmail.com>
2021-09-07 20:49:33 +0500

    error in useState
    setState is delayed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+//Components
+import NavBar from './components/NavBar';
+import InfoPanel from './components/InfoPanel';
+
+
+//STYLE
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <InfoPanel />
     </div>
   );
 }
